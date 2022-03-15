1b318f253ae2625192cf90802a022c1d4919dceb
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-03-15 19:01:38 -0400

    Add end_screen function to bin/main

--- wordle2/bin/main.ml
+++ wordle2/bin/main.ml
@@ -1,11 +1,29 @@
-let rec play () =
-  print_string "Enter your guess: ";
-  let input = read_line () in
-  match input with
-  | "" -> exit 0
-  | _ -> play ()
+(** Temporary word processor, to be replaced by functions from src files*)
+let naive_processor = function 
+| "camels" -> true
+| _ -> false
 
-let start () =
+let print_history () =
+  print_endline "\nHistory:\n(Currently Unimplemented)"
+
+let end_screen () =
+  print_endline "Score: (To be implemented)"
+
+(** [play ()] represents the in-game state. *)
+let rec play () =
+  print_string "\nEnter your guess: ";
+  let input = read_line () |> naive_processor in
+  match input with
+  | true -> 
+    print_endline "\n\nCongratulations! You have guessed the correct word!";
+    end_screen ()
+  | false ->
+    print_endline "\n\nIncorrect!";
+    print_history ();
+    play ()
+
+(** [start ()] represents the pre-game state. *)
+    let start () =
   print_endline "Welcome to Wordle 2.0!";
   print_endline "Guess the five-letter word.";
   play()
