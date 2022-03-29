2105cea1a1c36d6cab8856e57d20b0b5b929f9d5
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-03-29 02:03:06 -0400

    Hint functionality in history.ml

--- wordle2/src/history.ml
+++ wordle2/src/history.ml
@@ -1,15 +1,17 @@
+(** Representation of a guessed word. *)
 type guessed_word = {
   word : string;
   colorization : int list (* Colorization of the word as given by Processor.color_list*)
 }
 
 type hint = {
-  (* Id 0 = grey letter hint, Id 1 = yellow letter hint, Id 2 = green letter hint *)
+  (* id: 0, 1, 2 is for grey, yellow, and green letter hint, respectively. *)
   id : int;
   letter : string;
   position : int; (* -1 if position not provided in hint. *)
 }
 
+(** Representation of the colored keyboard. *)
 type keyboard = {
   unguessed_letters : string list;
   grey_letters : string list;
@@ -19,7 +21,7 @@ type keyboard = {
 
 type t = {
   (* guessed_words RI: the rightmost entry should be the earliest guessed word while the
-     leftmost is the latest. They should be ordered in increasing age.*)  
+     leftmost is the latest. They should be ordered in increasing age. *)  
   answer : string;
   guessed_words : guessed_word list;
   keyboard : keyboard;
@@ -42,15 +44,19 @@ let init_hist ans = {
 }
 
 
-(** returns keyboard with unguessed letter removed*)
+(** [remove_from_unguessed kb l] is [kb] with letter [l] removed from the
+    unguessed letters list. *)
 let remove_from_unguessed keyboard letter =
   let new_unguessed = List.filter (fun l -> l <> letter) keyboard.unguessed_letters in
   { keyboard with unguessed_letters = new_unguessed }
 
 
-(* update keyboard based on w's colorings *)
-let rec update_keyboard keyboard colorized_word =
-  match colorized_word with
+(** [update_keyboard kb color_wd] is [kb] with new colored letters based on
+    [color_wd]'s colorings.
+    Precondition: [color_wd] is in the format of the output of
+    Processor.colorize_guess. *)
+let rec update_keyboard keyboard colorized_guess =
+  match colorized_guess with
   | [] -> keyboard
   | (letter, value) :: t ->
     match value with
@@ -82,8 +88,7 @@ let rec update_keyboard keyboard colorized_word =
     | _ -> raise (Failure "Invalid value")
 
 
-(** [add_word w] adds word to the history. Precondition: [w] is a valid word according to the game configuration. *)
-let add_word hist w = 
+let add_guess hist w = 
   let colors = Processor.color_list hist.answer w in
   let new_guessed_word = { word = w; colorization = colors } in
   let new_guessed_words = new_guessed_word :: hist.guessed_words in
@@ -91,4 +96,47 @@ let add_word hist w =
   let new_keyboard = update_keyboard hist.keyboard colorized_word in
   { hist with guessed_words = new_guessed_words; keyboard = new_keyboard }
   
-  
+(** [add_hint_to_hist hist hint] is the history with the keyboard updated with
+    [hint]'s information. *)
+  let add_hint_to_hist hist hint = { hist with hints = hint :: hist.hints }
+(*TODO: Update keyboard with hint*)
+
+(** [unguessed_yellows hist] is the list of yellow unguessed letters in state
+    [hist]. *)
+let unguessed_yellows hist = 
+  let ungssd = hist.keyboard.unguessed_letters in
+  List.filter (fun l -> String.contains hist.answer l.[0]) ungssd
+
+(** [unguessed_greys hist] is the list of grey unguessed letters in state
+    [hist]. *)
+let unguessed_greys hist = 
+  let ungssd = hist.keyboard.unguessed_letters in
+  List.filter (fun l -> not (String.contains hist.answer l.[0])) ungssd
+
+(** [get_rand_ele lst] is a random element from [lst]. *)
+  let get_rand_ele lst =
+  let rand = Random.int (List.length lst) in
+  List.nth lst rand
+
+(* id is hint id (see History.hint) *)
+let get_hint id hist = match id with
+| 0 -> 
+  let ungssd = unguessed_greys hist in
+  let hint_letter = get_rand_ele ungssd in
+  let hint = { id = 0; letter = hint_letter; position = -1} in
+  let new_hist = add_hint_to_hist hist hint in
+  (Some hint, new_hist)
+| 1 ->
+  let ungssd = unguessed_yellows hist in
+  let hint_letter = get_rand_ele ungssd in
+  let hint = { id = 1; letter = hint_letter; position = -1} in
+  let new_hist = add_hint_to_hist hist hint in
+  (Some hint, new_hist)
+| 2 ->
+  let ungssd = unguessed_yellows hist in
+  let hint_letter = get_rand_ele ungssd in
+  let pos = String.index hist.answer hint_letter.[0] in
+  let hint = { id = 2; letter = hint_letter; position = pos} in
+  let new_hist = add_hint_to_hist hist hint in
+  (Some hint, new_hist)
+| _ -> (None, hist)
--- wordle2/src/history.mli
+++ wordle2/src/history.mli
@@ -3,7 +3,21 @@
     Game history includes: guess history, provided hints, keyboard colorings. *)
 
 type hint
+(** Representation of a hint. *)
 
 type t
+(** Representation of game history. *)
 
 val init_hist : string -> t
+(** [init_hist] is the history at the start of the game. *)
+
+val add_guess : t -> string -> t
+(** [add_guess w] is the history with guess [w] added.
+    Precondition: [w] is a valid word according to the game configuration. *)
+
+val get_hint : int -> t -> hint option * t
+(** [get_hint id hist] is a [(hint option, hist)] pair for the hint and new
+    history state. [hint option] is [Some hint] if there is a valid hint for
+    [id] and [None] otherwise. [id] is 0 to represent a grey letter hint,
+    1 to represent a yellow letter hint, and 2 to represent a green letter
+    hint. *)
