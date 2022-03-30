f56f44ecb6fb81af02b33ed7961d6a82e5da2218
jot-jt <91110564+jot-jt@users.noreply.github.com>
2022-03-29 22:01:25 -0400

    Code cleanup

--- wordle2/bin/main.ml
+++ wordle2/bin/main.ml
@@ -21,17 +21,7 @@ let rec colored_row column (letters : (string * int) list) : unit =
   | [] -> print_string "|"
   | h :: t -> print_string "| "; print_color_letter h; print_string " ";
     colored_row column t
-
-let keyboard = [["q"; "w"; "e"; "r"; "t"; "y"; "u"; "i"; "o"; "p"]; 
-  ["a"; "s"; "d"; "f"; "g"; "h"; "j"; "k"; "l"]; 
-  ["z"; "x"; "c"; "v"; "b"; "n"; "m"]]
-
-let rec guess_list guesses = match guesses with 
-| [] -> []
-| h :: t -> h :: guess_list t
-
-let make_keyboard guesses = assert false
-
+  
 let rec make_grid row column (guesses : ((string * int) list) list) : unit = 
   match guesses with 
   | [] -> empty_grid row column
@@ -42,7 +32,7 @@ let rec make_grid row column (guesses : ((string * int) list) list) : unit =
 the guesses so far.
 Precondition : length of guesses is smaller then dif*)
 let make_game dif letters guesses : unit =
-  print_endline "Wordle 2.0 ( i ) ( l ) ( h ) ( r )";
+  print_endline "Wordle 2.0 ( i ) ( l )";
   make_grid dif letters guesses
 
  
@@ -65,7 +55,7 @@ let rec print_word colored_word =
 
 (** [in_check str] check if the str is a valid word by comparing it to dict*)
 let in_check str :bool=
-  in_dict dict str && (str != "i") && (str != "l") && (str != "h") && (str != "r") 
+  in_dict dict str 
 
 
 let print_history guess =
@@ -88,21 +78,11 @@ let dif = 6
 (** represents the number of letters of the word the user has to guess. Will be
 replaces with implementation based on theselection by user *)
 let letters = 5
-let instructions = "\nInstructions:\nWelcome to Wordle 2.0, the goal of the game is to find the secret "^ string_of_int letters ^ " letter word.\n - To add a word into the game, type it into the terminal.\n - If the letter(s) in the word suggested is in the solution, but in the wrong position, it will come out as yellow.\n - If the letter(s) in the word suggested is in the solution, but in the correct position, it will come out as green.\n - If the letter(s) in the word suggested is not in the solution, it will come out as grey.\nThe end goal is to get the secret word in 6 tries or less.\n\nGood Luck!\n\n"
-let leaderboard = "\nLeaderboard:\nTO BE IMPLEMENTED\n\n"
-let hint = "\nHint:\nTO BE IMPLEMENTED\n\n"
-
-let igCommand inp = match inp with
-| "i" -> print_string(instructions)
-| "l" -> print_string(leaderboard)
-| "h" -> print_string(hint)
-| _ -> print_string("")
-
 
 (** [play ()] represents the in-game state. *)
 let rec play (guesses : ((string*int) list)list) () =
   make_game dif letters guesses;
-  let input = String.lowercase_ascii (read_line ()) in  
+  let input = read_line () in
   if (in_check input) then (
   let output = input |> naive_processor in
   match output with
@@ -112,11 +92,11 @@ let rec play (guesses : ((string*int) list)list) () =
     make_game dif letters (guesses @ [(colorize_guess correct_word input)]);
     end_screen false ()) 
     else play (guesses @ [(colorize_guess correct_word input)]) () )
-  else (if (input = "r") then (ANSITerminal.print_string [ ANSITerminal.Underlined ] "\n\nStarting New Game\n\n"; play [] ()) else if (input = "i" || input = "l" || input = "h") then igCommand(input) else  print_endline (input^" is not a valid word");
+  else (print_endline (input^" is not a valid word");
   play guesses ())
 
 (** [start ()] represents the pre-game state. *)
-    let start () =
+let start () =
   print_endline "Welcome to Wordle 2.0!";
   print_endline "Guess the five-letter word.";
   play [] ()
